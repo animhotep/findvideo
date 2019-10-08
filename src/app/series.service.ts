6c57b4b3f479c19656c10dc8e3f1a3cebff1b25c
pasha <animhotep@gmail.com>
2019-10-08 09:36:30 +0300

    2nd variant of service

--- src/app/series.service.ts
+++ src/app/series.service.ts
@@ -12,16 +12,15 @@ export class SeriesService {
   constructor(private http: HttpClient) {
   }
 
+  // https://metanit.com/web/angular2/6.2.php
   getUsers() {
     return this.http.get('https://reqres.in/api/users').pipe(
-      map((data: any) => {
-        return data.data.map(user => {
-          return {
-            id: user.id,
-            name: user.first_name + user.last_name
-          };
-        });
-      })
+      map((data: any) => (
+        data.data.map(user => ({
+          id: user.id,
+          name: user.first_name + user.last_name
+        }))
+      ))
     );
   }
 }
