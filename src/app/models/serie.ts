36c84a8cadc40a1192bf942a7fbf9b4070a75907
pasha <animhotep@gmail.com>
2019-12-17 20:48:05 +0200

    refactored

--- src/app/models/serie.ts
+++ src/app/models/serie.ts
@@ -1,4 +1,5 @@
 export interface Serie {
   id?: number;
   name: string;
+  asd: boolean
 }
--- src/app/series.service.ts
+++ src/app/series.service.ts
@@ -14,32 +14,32 @@ export class SeriesService {
 
   // https://metanit.com/web/angular2/6.2.php
   getUsers() {
-    return this.http.get('https://reqres.in/api/users').pipe(
-      map((r: any) => r.data.map(user => this.toUser(user)))
-    );
+    return this.http.get('https://reqres.in/api/users').pipe(map(
+      (r: any) => r.data.map(user => this.toUser(user))
+    ));
   }
 
-  private toUser(user): Serie {
+  private toUser(user) {
     return {
       id: user.id,
-      name: `ъъъъъ ${user.first_name}  ${user.last_name} ъъъъъъ`,
+      name: `ъъъъъ ${user.first_name}  ${user.last_name} ъъъъъъ`
     };
   }
-/*
 
-  getUsers() {
-    return fetch('https://reqres.in/api/users').then(r => r.json())
-      .then(data => {
-        return data.data.map(user => {
-          return {
-            id: user.id,
-            name: user.first_name + user.last_name
-          };
+  /*
+
+    getUsers() {
+      return fetch('https://reqres.in/api/users').then(r => r.json())
+        .then(data => {
+          return data.data.map(user => {
+            return {
+              id: user.id,
+              name: user.first_name + user.last_name
+            };
+          });
         });
-      });
-  }
-*/
-
+    }
+  */
 
 
 }
